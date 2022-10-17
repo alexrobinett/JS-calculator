6b3c72292d1c6ef88b32b94c94cbbbf4940493d6
robinetta <alxr19@gmail.com>
2022-10-17 19:33:17 -0400

    basic calculator functionality - some bugs to fix

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,6 @@
+let displayValue = ""
+let storedValue = ""
+let operator =
 // Functions for Math
 function add (num1 , num2) {
 	let result = num1 + num2
@@ -25,7 +28,7 @@ function divide(num1 , num2){
 
 // function to receive numbers and call the correct operator function 
 
-function operate(num1 , num2, operator){
+function operate(num1 , num2 = 0, operator){
 if (operator === "+"){
     console.log("I will add")
     return add(num1 , num2)
@@ -39,5 +42,48 @@ if (operator === "+"){
     console.log("I will Multiply")
     return multiply(num1 , num2)
 }else console.log("something went wrong")
+}
 
+
+const BUTTONS = document.querySelectorAll('button')
+
+const INPUT_NUMBER_DISPLAY = document.querySelector(".big-number")
+
+BUTTONS.forEach((button) => {
+    
+    button.addEventListener('click', () => {
+      if(button.classList == "num"){
+        updateDisplay()
+        displayValue += button.id
+        updateDisplay()
+        console.log(storedValue)
+      }else if(button.id == "clear"){
+        displayValue = 0
+        storedValue = 0
+        updateDisplay()
+        displayValue = ""
+      }else if(button.classList == "operations"){
+            operator = button.id
+            storedValue = operate(Number(storedValue) , Number(displayValue) , operator)
+            displayValue = storedValue
+            updateDisplay()
+            displayValue = ""
+        console.log(`the current number ${displayValue} and the stored value is ${storedValue}`)
+      }else if(button.id == "operate"){
+        displayValue = operate(Number(storedValue) , Number(displayValue) , operator)
+        if(isNaN(displayValue)){
+            displayValue = "Don't divide by zero dummy!"
+            updateDisplay()
+            displayValue = ""
+        }else
+        updateDisplay()
+        displayValue = ""
+      }
+     
+    });
+  });
+
+function updateDisplay(){
+    
+    INPUT_NUMBER_DISPLAY.textContent = displayValue
 }
